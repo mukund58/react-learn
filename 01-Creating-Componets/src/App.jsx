bef7b12261900cfbcfd6a46dbbccbc855ae8a155
mukund58 <cursedscripting@gmail.com>
2025-07-22 23:33:30 +0530

    add react day1 & day2

---
+++ 01-Creating-Componets/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default App
+function App() {
+
+  return (
+	  <h1>Hello, World!</h1>
+
+  )
+}
---
+++ 01-Creating-Componets/src/Nav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+export default Nav 
+function Nav(){
+	return(
+		<h1>Nav Bar</h1>
+	)
+}
